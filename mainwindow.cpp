64f251d200b904e4b90f83560327a82f79dc14d2
Jonhy <eljonyal101@gmail.com>
2025-07-29 21:44:33 -0500

    Agregado panel superior con logo y titulo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,9 +20,44 @@ MainWindow::MainWindow(QWidget *parent)
     mainIcon.addFile(":/icons/images/icons/mainLogo16x16.ico", QSize(16,16));
     mainIcon.addFile(":/icons/images/icons/mainLogo32x32.ico", QSize(32,32));
     mainIcon.addFile(":/icons/images/icons/mainLogo48x48.ico", QSize(48,48));
-    mainIcon.addFile(":/icons/images/icons/mainLogo256x256.ico", QSize(256,256));
-
+    mainIcon.addFile(":/icons/images/icons/mainLogo256x256.ico", QSize(256,256));        
     this->setWindowIcon(mainIcon);
+
+    //layout principal, eliminando márgenes
+    QVBoxLayout *mainLayout=new QVBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(0,0,0,0);
+
+    /**********parte superior, logo, nombre y versión del programa*********/
+    QWidget *headerWidget=new QWidget();
+    headerWidget->setFixedHeight(64);
+    headerWidget->setStyleSheet("background-color: #1F2B42;");
+    mainLayout->addWidget(headerWidget);
+
+    //layout del encabezado
+    QHBoxLayout *layoutHeader=new QHBoxLayout(headerWidget);
+
+    //logo
+    labelLogo=new QLabel();
+    QPixmap qpixLogo(":/icons/images/icons/mainLogo.png");
+    QPixmap qpixLogoScaled= qpixLogo.scaled(48,48,Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    labelLogo->setPixmap(qpixLogoScaled);
+    layoutHeader->addWidget(labelLogo,0,Qt::AlignVCenter);
+
+    //nombre
+    labelNombrePrograma=new QLabel("Pantroller");
+    labelNombrePrograma->setStyleSheet(R"(
+        font-size: 24px;
+        color: #4E7FE3;
+        font-weight: bold;
+    )");
+    layoutHeader->addWidget(labelNombrePrograma);
+    layoutHeader->addStretch();
+    layoutHeader->addWidget(new QLabel("Version 1.0"));
+    /***********************************************************************/
+
+
+    //subir todo hacia arriba
+    mainLayout->addStretch();
 
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,5 +19,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QLabel *labelLogo, *labelNombrePrograma;
+
 };
 #endif // MAINWINDOW_H
